7e5fb4b7e6245bb16744896de3db4a0d50bbeea4
Hiro Tanaka <HTanaka@lcs.on.ca>
2023-11-15 11:46:53 -0500

    Initial Commit

---
+++ CustomInterface/CustomInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomInterfaceApp.swift
+//  CustomInterface
+//
+//  Created by 田中宏貴 on 2023-11-15.
+//
+
+import SwiftUI
+
+@main
+struct CustomInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
